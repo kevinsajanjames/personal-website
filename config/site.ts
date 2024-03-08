de0d8ef97977a1401ffe1da93e64d657341e597f
Kevin Sajan James <kevinsajan@gmail.com>
2024-03-08 14:02:24 +0400

    Added siteConfig object

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Kevin's Blog",
+  url: "www.kevinsajanjames.com",
+  description: "Made with love and grace.",
+  author: "Kevin Sajan James",
+  links: {
+    twitter: "",
+    github: "",
+    email: "",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
